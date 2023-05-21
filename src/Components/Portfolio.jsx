1968a66db700f1fc674afa4d4ff2dd4cda7783e8
Emilly Victoria <82069194+emillyvictoria@users.noreply.github.com>
2023-05-21 04:33:15 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -30,17 +30,12 @@ const imageAltText = "desktop with books and laptop";
  * links section. Below is a sample, update to reflect links you'd like to highlight.
  */
 const projectList = [
+
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Breast Cancer Prediction Model",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
-  },
-  {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "A machine learning model that predicts if a patient has breast cancer based on a set of attributes.",
+    url: "https://github.com/emillyvictoria/MODELO-IA",
   },
   {
     title: "My Resume Site",
@@ -49,10 +44,10 @@ const projectList = [
     url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Netflix Website Clone",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "A Netflix website clone using ReactJS, HTML and CSS.",
+    url: "https://github.com/emillyvictoria/OSCARFLIX-CLONE",
   },
 ];
 
